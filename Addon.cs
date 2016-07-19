346057ef83e38249391f10f3e286aeeead47c1c6
Christian Bronk <chris.github@bronk.de>
2016-07-19 20:53:02 +0200

    Projektdateien hinzufügen.

---
+++ Addon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kOS;
+using kOS.Safe;
+using UnityEngine;
+using System.Reflection;
+
+using kOS.Safe.Encapsulation;
+
+namespace kOS.AddOns.kOSBiome
+{
+    [kOSAddon("BIOME")]
+    [kOS.Safe.Utilities.KOSNomenclature("BiomeAddon")]
+    public class Addon : Suffixed.Addon
+    {
+        private const BindingFlags BINDINGS = BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static;
+        public Addon(SharedObjects shared) : base(shared)
+        {
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("CURRENT", new kOS.Safe.Encapsulation.Suffixes.NoArgsSuffix<StringValue>(GetCurrentBiome, "Get Name of current Biome"));
+            AddSuffix("BIOMEAT", new kOS.Safe.Encapsulation.Suffixes.TwoArgsSuffix<StringValue, kOS.Suffixed.BodyTarget, kOS.Suffixed.GeoCoordinates>(GetBiomeAt, "Get Name of Biome of Body,GeoCoordinates"));
+        }
+
+        public override BooleanValue Available()
+        {
+            return true;
+        }
+        private StringValue GetCurrentBiome()
+        {
+           var vessel = FlightGlobals.ActiveVessel;
+           var body = FlightGlobals.ActiveVessel.mainBody;
+           return ScienceUtil.GetExperimentBiome(body, vessel.latitude, vessel.longitude);
+        }
+        private StringValue GetBiomeAt(kOS.Suffixed.BodyTarget body, kOS.Suffixed.GeoCoordinates coordinate)
+        {
+            return ScienceUtil.GetExperimentBiome(body.Body, coordinate.Latitude, coordinate.Longitude);
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("KOS-Biome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KOS-Biome")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("25e7a2c8-46a9-442d-a634-5e4a483ccd80")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssemblyDependency("kOS", 0, 20)]
